8e987e485888e413f108bc0dfec77c516ba505ba
chalee <695469701@qq.com>
2025-06-21 12:34:47 +0800

    feat: U

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
+const path = require("path");
 const nextConfig = {
   experimental: {
     appDir: true,
@@ -13,13 +13,19 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname, './'),
-      '@/components': path.resolve(__dirname, './components'),
-      '@/utils': path.resolve(__dirname, './utils'),
-      '@/styles': path.resolve(__dirname, './styles'),
-    }
-    return config
+      "@": path.resolve(__dirname, "./"),
+      "@/components": path.resolve(__dirname, "./components"),
+      "@/utils": path.resolve(__dirname, "./utils"),
+      "@/styles": path.resolve(__dirname, "./styles"),
+    };
+    return config;
   },
+  // 完全跳过静态优化，强制所有页面为动态
+  experimental: {
+    staticPageGenerationTimeout: 0,
+  },
+  // 输出配置
+  output: "standalone",
 };
 
 module.exports = nextConfig;
